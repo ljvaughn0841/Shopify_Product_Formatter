3955b6d32cef5bc4534dc647cbdb721e63b90422
Lou Vaughn <ljvaughn0841@eagle.fgcu.edu>
2022-10-05 11:21:18 -0400

    Improved readability in prototype.py.

--- prototype.py
+++ prototype.py
@@ -1,3 +1,10 @@
+"""
+Shopify Product Formatter Prototype
+This is a small prototype created to teach myself pandas and the tkinter GUI.
+The purpose of this program is to demonstrate how one might use python
+to reformat an inventory/product list into something that could be imported
+into shopify.
+"""
 from tkinter import *
 from tkinter import filedialog
 import pandas as pd
@@ -8,9 +15,10 @@ df = pd.DataFrame()
 
 # For more information on headers in shopify see here
 # https://help.shopify.com/en/manual/products/import-export/using-csv#product-csv-file-format
-shopify_header = ["Title", "Body (HTML)", "Vendor", "Tags", "Published", "Option1 Name", "Option1 Value", "Variant SKU",
-                  "Variant Inventory Tracker", "Variant Inventory Policy", "Variant Fulfillment Service",
-                  "Variant Price", "Status"]
+shopify_header = ["Title", "Body (HTML)", "Vendor", "Tags", "Published",
+                  "Option1 Name", "Option1 Value", "Variant SKU",
+                  "Variant Inventory Tracker", "Variant Inventory Policy",
+                  "Variant Fulfillment Service", "Variant Price", "Status"]
 
 root = Tk()
 root.title('Product Formatter')
@@ -27,6 +35,12 @@ my_tree = ttk.Treeview(my_frame)
 
 # Opens a file
 def open_file():
+    """
+    This function opens a .csv file then displays it on the screen
+    as a spreadsheet.
+
+    :return: None
+    """
     global df
     file = filedialog.askopenfilenames(
         initialdir="E:/Ram4Global/ProductFormatter",
@@ -57,7 +71,7 @@ def open_file():
             # Pack the treeview onto the screen
             my_tree.pack()
 
-            return df
+            return
 
         except ValueError:
             error_label.config(text="ERROR: The File Couldn't be Opened")
@@ -67,22 +81,33 @@ def open_file():
 
 
 def clear_tree():
+    """
+    clears the tree
+    """
     my_tree.delete(*my_tree.get_children())
 
 
-def apply_shopify_format():
+def test_funciton():
+    """
+    Test function doesn't actually do what the name implies.
+    Simply prints the shopify table links.
+    """
     print("apply format button")
     print(df)
     print("hello!")
     for i in range(len(DropDownLinks.req_array)):
-        print(DropDownLinks.tbl_array[i].get(), " links to ", DropDownLinks.req_array[i].get())
+        print(DropDownLinks.tbl_array[i].get(), " links to ",
+              DropDownLinks.req_array[i].get())
 
 
 class DropDownLinks:
-    # There are two lists containing input from the dropdowns
-    # These dropdowns are linked together by index
-    # The elements of the array are pointers to strings created with StringVar()
-    # The elements value can be retrieved with .get()
+    """
+    DropDownLinks contains two arrays from the "linked" dropdowns
+    These dropdowns are linked together by index
+    (i.e. requirement 3 links to table variable 3)
+    The elements of the array are pointers to strings created w/ StringVar()
+    The elements value can be retrieved with .get()
+    """
 
     # A list of the requirements that have been selected by the user
     req_array = []
@@ -91,6 +116,13 @@ class DropDownLinks:
 
 
 def add_link():
+    """
+    Creates two dropdown tables and adds the pointer to their input to
+    the DropDownLinks arrays.
+    It then packs the dropdowns onto the GUI.
+
+    :return: None
+    """
     print("Add link")
 
     # creates two dropdowns
@@ -118,6 +150,10 @@ def add_link():
 
 
 def remove_link():
+    """
+    Removes the last dropdown from the GUI and
+    pops them off of the DropDownLinks lists.
+    """
     print("removes a link")
 
     # removes the bottom two dropdowns from the link frame
@@ -125,24 +161,32 @@ def remove_link():
     slaves[0].destroy()
     slaves[1].destroy()
 
-    # Pops off the ends of the req and tbl array ( removes the dropdowns values from storage )
+    # Pops off the ends of the req and tbl array
+    # ( removes the dropdowns values from storage )
     DropDownLinks.req_array.pop()
     DropDownLinks.tbl_array.pop()
 
 
 def export_file():
+    """
+    Formats the file for shopify adding in the defaults then exports.
+    :return:
+    """
+    # TODO seperate export and formating of file into different functions
     print("Exporting Shopify Formatted File")
     new_shopify_df = pd.DataFrame(columns=shopify_header)
     print(new_shopify_df)
 
-    # Adding data to the new shopify list from the dropdown linked columns in imported file
+    # Adding data to the new shopify list from the
+    # dropdown linked columns in imported file
     for i in range(len(DropDownLinks.req_array)):
-        new_shopify_df[DropDownLinks.req_array[i].get()] = df[DropDownLinks.tbl_array[i].get()]
+        new_shopify_df[DropDownLinks.req_array[i].get()] = \
+            df[DropDownLinks.tbl_array[i].get()]
     print(new_shopify_df)
-    new_shopify_df.to_csv('new_shopify_file.csv', index=False)
 
     # Adding in the default variables
     # TODO make an array for defaults and make this into a loop
+
     if "Vendor" not in DropDownLinks.req_array:
         new_shopify_df["Vendor"] = "CleanMax"
     if "Published" not in DropDownLinks.req_array:
@@ -173,17 +217,21 @@ link_button = Button(button_frame, text="Add Link", command=lambda: add_link())
 link_button.grid(row=0, column=0, padx=10, pady=10)
 
 # Adds the remove link button
-link_button = Button(button_frame, text="Remove Link", command=lambda: remove_link())
+link_button = Button(button_frame, text="Remove Link",
+                     command=lambda: remove_link())
 link_button.grid(row=0, column=1, padx=10, pady=10)
 
-# Adds the format sheet button
-format_button = Button(button_frame, text="Format Sheet", command=lambda: apply_shopify_format())
+# Adds the test sheet button
+format_button = Button(button_frame, text="Test Sheet",
+                       command=lambda: test_funciton())
 format_button.grid(row=0, column=2, padx=10, pady=10)
 
+# Makes a frame for the map links
 link_frame = LabelFrame(root, text="Mapping Links")
 link_frame.pack(fill="x", expand=0, padx=20)
 
-# TODO make an area where users can select default variables and assign them valid values
+# TODO make an area where users can select
+#  default variables and assign them valid values
 
 # Label for errors
 error_label = Label(root, text='')
